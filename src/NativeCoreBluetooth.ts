80523d00376fb4374e41d89822c40b151d5b2ad3
Lucian <lucian@luxtud.io>
2023-01-26 00:00:32 +0900

    feat: defined CoreBluetooth JSI

--- src/NativeCoreBluetooth.ts
+++ src/NativeCoreBluetooth.ts
@@ -1,8 +1,55 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
+interface GattServices {
+  serviceIds: string[];
+  characteristics: string[];
+  descriptors: string[];
+}
+
+interface CBCharacteristicData {
+  uuid: string;
+  data: string;
+  descriptors?: undefined;
+}
+
+interface CBDescriptorData {
+  uuid: string;
+  data: string;
+}
+
 export interface Spec extends TurboModule {
-  multiply(a: number, b: number): Promise<number>;
+  startScan(): Promise<void>;
+  startScanByCompanyId(companyIds: number[]): Promise<void>;
+  stopScan(): Promise<void>;
+  isDiscovering(): Promise<boolean>;
+  checkAdvertisePermission(): Promise<boolean>;
+  checkScanPermission(): Promise<boolean>;
+  connect(identifier: string): void;
+  disconnect(): void;
+  discoverServices(): Promise<GattServices>;
+  writeCharacteristic(
+    serviceId: string,
+    uuid: string,
+    data: string
+  ): Promise<void>;
+  readCharacteristic(
+    serviceId: string,
+    uuid: string
+  ): Promise<CBCharacteristicData>;
+  writeDescriptor(
+    serviceId: string,
+    characteristicId: string,
+    uuid: string,
+    data: string
+  ): Promise<void>;
+  readDescriptor(
+    serviceId: string,
+    characteristicId: string,
+    uuid: string
+  ): Promise<CBDescriptorData>;
+  requestAdvertisePermission(): Promise<void>;
+  requestScanPermissions(): Promise<void>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('CoreBluetooth');
